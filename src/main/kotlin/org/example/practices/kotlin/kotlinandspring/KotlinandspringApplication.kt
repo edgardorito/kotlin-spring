7254b9b0bf578fbf08fd6fcd5f13d1e2ff6b48d1
edgardorito <edgardorito@gmail.com>
2024-08-17 12:10:51 -0600

    First commit

---
+++ src/main/kotlin/org/example/practices/kotlin/kotlinandspring/KotlinandspringApplication.kt
@@ -0,0 +1,11 @@
+package org.example.practices.kotlin.kotlinandspring
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinandspringApplication
+
+fun main(args: Array<String>) {
+    runApplication<KotlinandspringApplication>(*args)
+}
---
+++ src/test/kotlin/org/example/practices/kotlin/kotlinandspring/KotlinandspringApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.example.practices.kotlin.kotlinandspring
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinandspringApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
